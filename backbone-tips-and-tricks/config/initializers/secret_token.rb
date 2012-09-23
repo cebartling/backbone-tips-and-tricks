ef27ea21bbe9ef4caede0d27b71358d76b9f54af
Christopher Bartling <chris.bartling@gmail.com>
2012-09-23 17:23:23 -0500

    Initial Rails app setup.

---
+++ backbone-tips-and-tricks/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneTipsAndTricks::Application.config.secret_token = 'e5420b32ed6e5fd2277b0b87e3a28f38cb0dc2580440fdb76a82b9ed3a18fd706f9f19292c695ffea9de6e2652e804421af16ecbd7fec857b002805db3ade8ac'
